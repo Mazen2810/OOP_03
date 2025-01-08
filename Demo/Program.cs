fe8a47a39413004db2e5d623ef8e3efef6554fb4
Mazen2810 <mazenmuhammed52@gmail.com>
2025-01-08 23:40:35 +0200

    OOP Pillars 3. PolyMorphism 1. Overloading

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -2,9 +2,42 @@
 {
     internal class Program
     {
+        static int sum(int X, int Y)
+        {
+            return X + Y;
+        }
+
+        static double sum(double X, double Y)
+        {
+            return X + Y;
+        }
+        static int sum(int X, int Y, int Z)
+        {
+            return X + Y + Z;
+        }
+        static double sum(int X, double Y)
+        {
+            return X + Y;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region OOP Pillars 3. PolyMorphism 1. Overloading
+            //dynamic result = sum(2.2, 2.2);
+            //result = sum(1, 2, 3);
+
+            //Console.WriteLine($"Result = {result}");
+
+            //Console.WriteLine("Ahmed");
+            //Console.WriteLine("A");
+            //Console.WriteLine(10);
+            //Console.WriteLine(true);
+
+            //Console.WriteLineString();
+            //Console.WriteLineChar();
+            //Console.WriteLineInt();
+            //Console.WriteLineBoolean(); 
+            #endregion
         }
     }
 }
