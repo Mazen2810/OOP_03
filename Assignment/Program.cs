3bfa203fa0eb505c5f42fc41174284469b2d0f8b
Mazen2810 <mazenmuhammed52@gmail.com>
2025-01-11 16:48:00 +0200

    Part01 Q2

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,4 +1,7 @@
-﻿using Assignment.Part01_Q1;
+﻿using System.Drawing;
+using Assignment.Part01_Q1;
+using Assignment.Part01_Q2;
+
 
 namespace Assignment
 {
@@ -13,14 +16,18 @@ namespace Assignment
             //Console.WriteLine(calc.Sum(10, 20));
             //Console.WriteLine(calc.Sum(10, 20, 30));
             //Console.WriteLine(calc.Sum(10.1, 20.2));
-            
-            
-            
+
+
+
 
 
             #endregion
 
-
+            #region Q2: Create a class named Rectangle with the following constructors
+            //Part01_Q2.Rectangle rec1 = new Part01_Q2.Rectangle();  // Named the path to avoid confusion between system.rectangle and  Part01_Q2.Rectangle 
+            //Part01_Q2.Rectangle rec2 = new Part01_Q2.Rectangle(10,20);
+            //Part01_Q2.Rectangle rec3 = new Part01_Q2.Rectangle(30);
+            #endregion
 
             #endregion
         }
