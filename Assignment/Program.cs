503533c21b7d5cf6ad2ad1fa2f555d440b8d8fa7
Mazen2810 <mazenmuhammed52@gmail.com>
2025-01-11 16:34:20 +0200

    Part01 Q1

---
+++ Assignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using Assignment.Part01_Q1;
+
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Part 01
+
+            #region Q1: Write a class named Calculator that contains a method named Add
+            //Calculator calc = new Calculator();
+            //Console.WriteLine(calc.Sum(10, 20));
+            //Console.WriteLine(calc.Sum(10, 20, 30));
+            //Console.WriteLine(calc.Sum(10.1, 20.2));
+            
+            
+            
+
+
+            #endregion
+
+
+
+            #endregion
+        }
+    }
+}
